7ad973cbdbde769f51b0d0a79ed6510ba4fe2f8a
NotZeetaa <rodrigo2005contente@gmail.com>
2021-04-14 17:38:28 +0100

    Lmi Config
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,14 +4,14 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=neXus-Lavender-Hmp-X
+kernel.string=neXus-lmi-V0.1
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=lavender
-supported.versions=9 - 11
+device.name1=lmi
+supported.versions=10 - 12
 supported.patchlevels=
 '; } # end properties
 
