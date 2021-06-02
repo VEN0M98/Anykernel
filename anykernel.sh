514f998b305abcd9e4cad2f4e68c0ddf6bb6bf7c
NotZeetaa <rodrigo2005contente@gmail.com>
2021-06-03 00:41:53 +0100

    Rebase
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,22 +4,21 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=neXus-lmi-V0.1
-do.devicecheck=1
+kernel.string=neXus_Lmi_V1
+do.devicecheck=0
 do.modules=0
-do.systemless=1
+do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
 device.name1=lmi
-supported.versions=10 - 12
-supported.patchlevels=
+device.name2=lmipro
+supported.versions=11
 '; } # end properties
 
 # shell variables
-block=/dev/block/bootdevice/by-name/boot;
+block=/dev/block/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
-
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
@@ -28,8 +27,5 @@ ramdisk_compression=auto;
 ## AnyKernel install
 dump_boot;
 
-# end ramdisk changes
-
 write_boot;
 ## end install
-
