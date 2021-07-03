eb881861ee79ae15842d1be120c1ae336b2525f8
NotZeetaa <rodrigo2005contente@gmail.com>
2021-07-03 18:13:05 +0100

    Rebase
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -5,7 +5,7 @@
 # begin properties
 properties() { '
 kernel.string=neXus_kernel_lmi
-do.devicecheck=0
+do.devicecheck=1
 do.modules=0
 do.systemless=0
 do.cleanup=1
@@ -26,6 +26,29 @@ ramdisk_compression=auto;
 
 ## AnyKernel install
 dump_boot;
+
+case "$ZIPFILE" in
+  *66fps*|*66hz*)
+    ui_print "  • Setting 66 Hz refresh rate"
+    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=1"
+    ;;
+  *69fps*|*69hz*)
+    ui_print "  • Setting 69 Hz refresh rate"
+    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=2"
+    ;;
+  *72fps*|*72hz*)
+    ui_print "  • Setting 72 Hz refresh rate"
+    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=3"
+    ;;
+  *77fps*|*77hz*)
+    ui_print "  • Setting 77 Hz refresh rate"
+    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=4"
+    ;;
+  *)
+    ui_print "  • Setting 60 Hz refresh rate"
+    patch_cmdline "msm_drm.framerate_override" ""
+    ;;
+esac
 
 write_boot;
 ## end install
