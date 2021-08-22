e3e3b79d6a530e6dff3d7228ff53e8158f07f9c7
NotZeetaa <rodrigo2005contente@gmail.com>
2021-08-22 16:48:54 +0100

    tree: Rebase to alioth
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -1,58 +1,49 @@
-# AnyKernel3 Ramdisk Mod Script
+	1	# AnyKernel3 Ramdisk Mod Script
 # osm0sis @ xda-developers
 
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=neXus_kernel_lmi
+kernel.string=Tertis kernel v2 by believer
 do.devicecheck=1
 do.modules=0
-do.systemless=0
+do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=lmi
-device.name2=lmipro
-supported.versions=11
+device.name1=alioth
+device.name2=aliothin
+device.name3=
+device.name4=
+device.name5=
+supported.versions=
 '; } # end properties
 
 # shell variables
-block=/dev/block/by-name/boot;
-is_slot_device=0;
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=1;
 ramdisk_compression=auto;
+
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+set_perm_recursive 0 0 750 750 $ramdisk/*;
+
+
 ## AnyKernel install
 dump_boot;
 
-case "$ZIPFILE" in
-  *66fps*|*66hz*)
-    ui_print "  • Setting 66 Hz refresh rate"
-    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=1"
-    ;;
-  *69fps*|*69hz*)
-    ui_print "  • Setting 69 Hz refresh rate"
-    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=2"
-    ;;
-  *72fps*|*72hz*)
-    ui_print "  • Setting 72 Hz refresh rate"
-    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=3"
-    ;;
-  *77fps*|*77hz*)
-    ui_print "  • Setting 77 Hz refresh rate"
-    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=4"
-    ;;
-  *80fps*|*80hz*)
-    ui_print "  • Setting 80 Hz refresh rate"
-    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=5"
-    ;;
-  *)
-    ui_print "  • Setting 60 Hz refresh rate"
-    patch_cmdline "msm_drm.framerate_override" ""
-    ;;
-esac
+# Begin Ramdisk Changes
+
+# migrate from /overlay to /overlay.d to enable SAR Magisk
+if [ -d $ramdisk/overlay ]; then
+  rm -rf $ramdisk/overlay;
+fi;
 
 write_boot;
 ## end install
+
